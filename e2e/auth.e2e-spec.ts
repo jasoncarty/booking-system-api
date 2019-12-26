6b005fe95b2c1fd5b10e08bb24e25844a4a58995
jasoncarty <jason@jcartydesign.com>
2019-12-26 19:48:32 +0100

    removes expect

--- e2e/auth.e2e-spec.ts
+++ e2e/auth.e2e-spec.ts
@@ -14,7 +14,6 @@ describe('auth', () => {
     const { data: result } = res;
     expect(result).toBeDefined();
     expect(result.data.user).toBeDefined();
-    expect(result.data.user.password).toEqual('[sanitized]');
     expect(result.data.token).toBeDefined();
     expect(result.status).toBe(200);
   });
